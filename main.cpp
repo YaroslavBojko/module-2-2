80364580d5d3892348b3a057fb16a4274f7306e6
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-15 14:48:06 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int productCost = 5000;
+    int delivery = 200;
+    int discount = 700;
+
+    int fullCost = productCost + delivery - discount;
+
+    std::cout << "The cost of your product is " << productCost << ", delivery will cost you " << delivery <<
+                 ", your discount becomes " << discount << ". Taking this into account, the total cost of the product: "
+                 << fullCost << std::endl;
 }
